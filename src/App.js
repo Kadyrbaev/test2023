02ff22727691a4fa8fc276f15a36aeceec348d8e
Kadyrbaev <sadyr_9494@mail.ru>
2023-06-28 02:47:46 +0600

    development

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <h1>HELLO</h1>
-      <h1>NEW BRANCH 5</h1>
+      <h1>DEVELOPMENT</h1>
       <Header/>
     </div>
   );
